d913e342911653f04619a3830d04d196da27046b
dxxidlee <dxxidlee@gmail.com>
2025-10-04 00:50:32 -0400

    PUSH

--- src/app/breakroom/page.tsx
+++ src/app/breakroom/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 export default function BreakRoomPage() {
   useEffect(() => {
     // Redirect to the local breakroom-vr index.html
-    window.location.href = '/breakroom-vr/index.html';
+    window.location.href = '/breakroom-vr/';
   }, []);
 
   return (
