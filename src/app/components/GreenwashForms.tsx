b1c804e06fd4f5a2da6a91a780cc8ac0fac67e0d
dxxidlee <dxxidlee@gmail.com>
2025-10-12 02:08:51 -0400

    PUSH

--- src/app/components/GreenwashForms.tsx
+++ src/app/components/GreenwashForms.tsx
@@ -249,7 +249,8 @@ const GreenwashForms: React.FC<GreenwashFormsProps> = ({ isOpen, onClose }) => {
             msOverflowStyle: 'none'
           }}
         >
-          <div className={isMobile ? 'p-6' : 'grid grid-cols-2 gap-3 items-start content-start p-6'}>
+          <div className="p-6">
+            <div className={isMobile ? '' : 'grid grid-cols-2 gap-3 items-start content-start'}>
           {/* Authorization Form */}
           {(isMobile ? activeTab === 'auth' : true) && (
           <div 
@@ -690,6 +691,7 @@ const GreenwashForms: React.FC<GreenwashFormsProps> = ({ isOpen, onClose }) => {
             </div>
           </div>
           )}
+            </div>
           </div>
         </div>
       </div>
